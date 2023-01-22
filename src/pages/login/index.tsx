e6bb69e4a5ae1ecffa23f568285f1d415f4c0b93
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-22 14:30:27 -0300

    feat: creation of the initial structure of the login and adjustment in the standardization of the forms

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,22 @@
+import Logo from "../../assets/Logo.png"
+
 const Login =  () => {
-    return <h1>Login</h1>
+    return (
+        <main>
+            <img src={Logo} alt="Logo Kenzie Hub"/>
+            <section>
+                <form action="">
+                    <label htmlFor="emailLogin">Email</label>
+                    <input id="emailLogin" type="text" placeholder="Digite aqui seu email"/>
+                    <label htmlFor="passwordLogin">Senha</label>
+                    <input id="passwordLogin" type="password" placeholder="Digite aqui sua senha"/>
+                    <button>Entrar</button>
+                </form>
+                <span>Ainda não possui uma conta?</span>
+                <button>Cadastre-se</button>
+            </section>
+        </main>
+    )
 }
 
 export default Login
