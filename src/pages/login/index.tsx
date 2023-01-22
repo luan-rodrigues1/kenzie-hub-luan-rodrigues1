99d58316e93499a10e3c9e9e1aca05648fc5b273
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-22 17:04:38 -0300

    fix: added validation in the login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,30 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import Logo from "../../assets/Logo.png"
+import { UserContext } from "../../contexts/UserContext";
+import { ILoginUser } from "../../contexts/UserContext";
+import { formLoginSchema } from "../../schemas/user.schemas";
 
 const Login =  () => {
+
+    const { loginUser } = useContext(UserContext);
+
+    const {register, handleSubmit, formState: { errors }} = useForm<ILoginUser>({
+        resolver: yupResolver(formLoginSchema),
+    });
+
     return (
         <main>
             <img src={Logo} alt="Logo Kenzie Hub"/>
             <section>
-                <form action="">
+                <form action="" onSubmit={handleSubmit(loginUser)}>
                     <label htmlFor="emailLogin">Email</label>
-                    <input id="emailLogin" type="text" placeholder="Digite aqui seu email"/>
+                    <input id="emailLogin" type="text" placeholder="Digite aqui seu email" {...register("email")}/>
+                    <p className="headline">{errors.email?.message}</p>
                     <label htmlFor="passwordLogin">Senha</label>
-                    <input id="passwordLogin" type="password" placeholder="Digite aqui sua senha"/>
+                    <input id="passwordLogin" type="password" placeholder="Digite aqui sua senha" {...register("password")}/>
+                    <p className="headline">{errors.password?.message}</p>
                     <button>Entrar</button>
                 </form>
                 <span>Ainda não possui uma conta?</span>
